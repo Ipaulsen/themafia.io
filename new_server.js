ec5b4f7a7a39a0e304cbced96db86f5344145256
Chase Roberts <crobertsbmw@gmail.com>
2016-07-22 16:46:11 -0600

    Changing the structure of join_codes and game_sockets

--- new_server.js
+++ new_server.js
@@ -5,9 +5,10 @@ var express = require('express'),//web frame work
     util = require('util'),
     server  = http.createServer(app),
     port    = 8080,
-    game_sockets = {};
-    join_codes = [];
+    game_sockets = {}; //socket_id is the key, socket_dict is the value
+    join_codes = {}; //4 digit code is the key, socket_id is the value
 
+var ALL_BOARDS = {}; //key value pairs. GameBoard objects as values, socket_id's as keys.
 server.listen(port);
 
 // Serve static files under the /public directory
@@ -25,27 +26,13 @@ app.get('/raos', function(req, res) {
 	//3 options for request origins 1.Game Board 2.Join Code 3.Direct controller Url.
 	var requestQuery = req.query;
 	//if this is a join code route them to the right game board
-	if(requestQuery.code){
-		if(requestQuery.code.length == 4){
-			console.log(4);
-			console.log(requestQuery.code);
-			for (var key in game_sockets) {
-				if(requestQuery.code == game_sockets[key].join_code){
-					var keyCovert = key;
-					keyCovert.toString();
-					console.log("key covert 1: "+keyCovert);
-					keyCovert = keyCovert.replace("/#", "");
-					console.log("key covert 2: "+keyCovert);
-					// res.sendFile(__dirname + '/raos.html?id='+keyCovert);
-					res.redirect("/raos?id="+keyCovert);
-				}
-			    // console.log(i+"st jc: "+ util.inspect(game_sockets[key].join_code));
-			    // console.log(i+"st key: "+ util.inspect(key));
-			    // i++;
-			}
-			//if wrong join code send them back to index
-			res.redirect('/raos?error=code_not_found');
-		}
+	if(requestQuery.code && join_codes[requestQuery.code]){
+				var keyCovert = join_codes[requestQuery.code];
+				keyCovert = keyCovert.replace("/#", "");
+				res.redirect("/raos?id="+keyCovert);
+
+		//if wrong join code send them back to index
+		res.redirect('/raos?error=code_not_found');
 	}
 	else{
 		res.sendFile(__dirname + '/raos.html');
@@ -63,7 +50,6 @@ var GameBoard = function(name) {
   	this.open = true; //a boolean for if we are accepting new players in this game or not.
 };
 
-var ALL_BOARDS = {} //key value pairs. GameBoard objects as values, socket_id's as keys.
 
 //when someone connects (game board or controller)
 io.sockets.on('connection', function (socket) { 
@@ -76,20 +62,13 @@ io.sockets.on('connection', function (socket) {
 		console.log("Game connected");
 		var join_code = Math.floor(1000 + Math.random() * 9000);
 		//taken the random 4 digit join code and see if it is taken 
-		var checkIfJoinCodeIsTaken = function(join_code_param){
-			console.log("boolean join code status: "+join_codes.indexOf(join_code) > -1);
-			if(join_codes.indexOf(join_code) > -1){
-				join_code = Math.floor(1000 + Math.random() * 9000);
-				checkIfJoinCodeIsTaken(join_code);
-			}
+		while (join_codes[join_code]){
+			join_code = Math.floor(1000 + Math.random() * 9000);
 		}
-		checkIfJoinCodeIsTaken(join_code);
-		game_sockets[socket.id] = {
-			socket: socket,
-			join_code: join_code
-		};
+		join_codes[join_code] = socket.id;
+		game_sockets[socket.id] = socket;
+
 		socket.emit("game_connected");
-		console.log(util.inspect(game_sockets));
 	});
 	//there will be no controller id param if there has not been one created.
 	socket.on('controller_connect', function(game_id,controller_id){
@@ -97,7 +76,7 @@ io.sockets.on('connection', function (socket) {
 		//append game socket with /# (this was appended on the game socket creation by socket.io)
 		game_id = "/#"+game_id;
 		if (game_sockets[game_id]){
-			//we will need another if statment to check to see if the controller is allready taken.
+			//we will need another if statement to check to see if the controller is already taken.
 			//This message is going to be sent when the user selects "join a game", and then inputs the game id.
 			//look up the game board and make sure that it's 'open'.
 			//add a controller to the game board
@@ -113,9 +92,7 @@ io.sockets.on('connection', function (socket) {
 	});
 
     socket.on('notify_controllers', function(){ //a game board connected.
-
         //we could change this to 'notify_controller'. Basically we need a function to pass messages from the board to one or all of the controllers.
-
     });
 
     socket.on('notify_board', function(){ //a game board connected.
